c4b1e3a39eb90ada5be1fa07aaf8e6799888f795
Heitor Polidoro <heitor.polidoro@gmail.com>
2023-12-19 16:50:53 -0300

    test

---
+++ tests/test_app.py
@@ -0,0 +1,23 @@
+from unittest.mock import patch
+
+import pytest
+
+from app import create_branch_handler
+
+
+@pytest.fixture
+def repo():
+    with patch("github.Repository.Repository") as Repository:
+        repository = Repository()
+        repository.default_branch = "master"
+        yield repository
+
+
+def test_create_pr(repo):
+    create_branch_handler()
+    repo.create_pull.assert_called_once_with(
+        "master",
+        "feature",
+        title="feature",
+        body="PR automatically created",
+        draft=False)
