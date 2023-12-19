03bd571d3295c17d631189350e7f83722061ee7e
Heitor Polidoro <heitor.polidoro@gmail.com>
2023-12-19 17:02:55 -0300

    fix test

---
+++ test_app.py
@@ -0,0 +1,16 @@
+from unittest.mock import Mock
+
+from app import create_branch_handler
+
+
+def test_create_pr():
+    event = Mock()
+    event.repository.default_branch = "master"
+    event.ref = "feature"
+    create_branch_handler(event)
+    event.repository.create_pull.assert_called_once_with(
+        "master",
+        "feature",
+        title="feature",
+        body="PR automatically created",
+        draft=False)
