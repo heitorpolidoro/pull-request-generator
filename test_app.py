27b24550542a54939dac9351d244c3da037f2343
Heitor Polidoro <heitor.polidoro@gmail.com>
2023-12-26 14:16:06 -0300

    test(CreatePR): split event fixture em more fixtures

--- test_app.py
+++ test_app.py
@@ -10,18 +10,40 @@ from app import app, create_branch_handler
 
 
 @pytest.fixture
-def event():
+def repository():
     """
-    This fixture creates a mock event object with the default branch set to 'master' and the ref set to 'feature'.
-    It returns the mock event object.
+    This fixture returns a mock repository object with default values for the attributes.
+    :return: Mocked Repository
+    """
+    repository = Mock()
+    repository.default_branch = "master"
+    repository.full_name = "heitorpolidoro/pull-request-generator"
+    repository.get_pulls.return_value = []
+    return repository
+
+
+@pytest.fixture
+def issue():
+    """
+    This fixture returns a mock issue object with default values for the attributes.
+    :return: Mocked Issue
+    """
+    issue = Mock()
+    issue.title = "feature"
+    issue.body = "feature body"
+    issue.number = 42
+    return issue
+
+
+@pytest.fixture
+def event(repository, issue):
+    """
+    This fixture returns a mock event object with default values for the attributes.
+    :return: Mocked Event
     """
     event = Mock()
-    event.repository.default_branch = "master"
-    event.repository.full_name = "heitorpolidoro/pull-request-generator"
-    event.repository.get_pulls.return_value = []
-    event.repository.get_issue.return_value.title = "feature"
-    event.repository.get_issue.return_value.body = "feature body"
-    event.repository.get_issue.return_value.number = 42
+    event.repository = repository
+    event.repository.get_issue.return_value = issue
     event.ref = "issue-42"
     return event
 
